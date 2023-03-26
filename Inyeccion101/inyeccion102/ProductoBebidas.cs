9ec252393daab898fa3091e23b6b209d30899f91
HWO <hwong24@hotmail.com>
2023-03-26 12:08:44 -0500

    visual2013

---
+++ Inyeccion101/inyeccion102/ProductoBebidas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inyeccion101
+{
+    class ProductoBebidas : IProducto
+    {
+        public string descripcion = "Bebidas";
+
+        string IProducto.getDescripcion()
+        {
+            return this.descripcion;
+        }
+    }
+}
---
+++ Inyeccion101/inyeccion102/Tienda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inyeccion101
+{
+    class Tienda
+    {
+        public string descripcion;
+        public IProducto producto;
+
+        public Tienda (string desc, IProducto prodc)
+        {
+            this.descripcion = desc;
+            this.producto = prodc;
+        }
+    }
+}
